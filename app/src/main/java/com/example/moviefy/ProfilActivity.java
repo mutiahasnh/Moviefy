78f5270786355c347e050285211edfadd3fbf44d
Mutiahasnh <mutiahasnh@gmail.com>
2021-09-10 08:14:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviefy/ProfilActivity.java
@@ -0,0 +1,67 @@
+package com.example.moviefy;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    protected Cursor cursor;
+    DatabaseHelper dbHelper;
+    SQLiteDatabase db;
+    SessionManager session;
+    String name, email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        dbHelper = new DatabaseHelper(this);
+
+        session = new SessionManager(getApplicationContext());
+
+        HashMap<String, String> user = session.getUserDetails();
+        email = user.get(SessionManager.KEY_EMAIL);
+
+        db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM TB_USER WHERE username = '" + email + "'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            cursor.moveToPosition(0);
+            name = cursor.getString(2);
+        }
+
+        TextView lblName = findViewById(R.id.lblName);
+        TextView lblEmail = findViewById(R.id.lblEmail);
+
+        lblName.setText(name);
+        lblEmail.setText(email);
+
+        setupToolbar();
+
+    }
+
+    private void setupToolbar() {
+       /* Toolbar toolbar =getActionBar();
+        toolbar.setTitle("Identitas Penyewa");
+        setSupportActionBar(toolbar);*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
